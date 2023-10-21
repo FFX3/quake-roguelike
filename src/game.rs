bac375bb29fe6481b7bbb2612baf5dd9a6106ab0
Justin <Justin McIntyre>
2023-10-21 18:34:55 -0300

    bunny hopping functional

--- src/game.rs
+++ src/game.rs
@@ -1,19 +1,6 @@
-use crate::insert_dependant_component;
-use bevy::{prelude::*, input::mouse::MouseMotion};
+use bevy::{prelude::*, input::mouse::MouseMotion, window::NormalizedWindowRef};
 use bevy_rapier3d::prelude::*;
 use bevy::transform::components::Transform;
-
-#[derive(Component, Reflect, Default, Debug)]
-#[reflect(Component)]
-pub enum SoundMaterial {
-    Metal,
-    Wood,
-    Rock,
-    Cloth,
-    Squishy,
-    #[default]
-    None,
-}
 
 #[derive(Component, Reflect, Default, Debug)]
 #[reflect(Component)]
@@ -22,65 +9,167 @@ pub struct Player;
 #[reflect(Component)]
 pub struct PlayerCamera;
 
-#[derive(Component, Reflect, Default, Debug)]
-#[reflect(Component)]
-/// Demo component showing auto injection of components
-pub struct ShouldBeWithPlayer;
+macro_rules! get_single_mut {
+    ($x:expr) => {
+        {
+            let result = $x.get_single_mut();
+            if result.is_err() { 
+                return; 
+            }
+            result.unwrap()
+        }
+    };
+}
 
-#[derive(Component, Reflect, Default, Debug)]
-#[reflect(Component)]
-/// Demo marker component
-pub struct Interactible;
+macro_rules! get_single {
+    ($x:expr) => {
+        {
+            let result = $x.get_single();
+            if result.is_err() { 
+                return; 
+            }
+            result.unwrap()
+        }
+    };
+}
 
-#[derive(Component, Reflect, Default, Debug)]
-#[reflect(Component)]
-/// Demo marker component
-pub struct Pickable;
-
-fn player_move_demo(
+fn player_move(
     keycode: Res<Input<KeyCode>>,
-    mut players_query: Query<&mut Transform, With<Player>>,
+    mut players_query: Query<(&mut KinematicCharacterController, Option<&mut KinematicCharacterControllerOutput>), With<Player>>,
     mut player_cameras_query: Query<&mut Transform, (With<PlayerCamera>, Without<Player>)>,
-    mut ev_motion: EventReader<MouseMotion>,
+    ev_motion: EventReader<MouseMotion>,
     primary_window: Query<&Window>,
+    time: Res<Time>,
 ) {
-    let player_result = players_query.get_single_mut();
-    if player_result.is_err() { return; }
-    let player_camera_result = player_cameras_query.get_single_mut();
-    if player_camera_result.is_err() { return; }
-    let primary_window_result = primary_window.get_single();
-    if player_camera_result.is_err() { return; }
+    let sensitivity = 5.0; 
+    let friction = 1.0; 
+    let max_accel = 32.0;
+    let run_speed = 18.0;
+    let stop_speed = 20.0;
+    let gravity = 30.;
+    let jump_force = 10.;
 
-    let window = primary_window_result.unwrap();
-    let mut player = player_result.unwrap();
-    let mut player_camera = player_camera_result.unwrap();
+    let (mut controller, controller_output_option) = get_single_mut!(players_query);
+    let player_camera = get_single_mut!(player_cameras_query);
+    let window = get_single!(primary_window);
 
-    let speed = 0.2;
+    let (current_velocity, grounded) = {
+        if let Some(controller_output) = controller_output_option {
+            (
+                controller_output.effective_translation / time.delta_seconds(),
+                controller_output.grounded,
+            )
+        } else { (Vec3::ZERO, false) }
+    };
 
-    let f = player_camera.forward();
-    let bearing_vector = Vec3::new(f.x, 0.0, f.z).normalize();
-    if keycode.pressed(KeyCode::W) {
-        player.translation += speed * bearing_vector;
-    }
+    println!("{}", current_velocity.length());
 
-    if keycode.pressed(KeyCode::A) {
-        player.translation += Vec3::new(bearing_vector.z, 0.0, bearing_vector.x * -1.0) * speed;
-    }
+    let current_velocity_xz = Vec3::new(
+        current_velocity.x,
+        0.0,
+        current_velocity.z,
+    );
 
-    if keycode.pressed(KeyCode::D) {
-        player.translation += Vec3::new(bearing_vector.z * -1.0, 0.0, bearing_vector.x) * speed;
-    }
+    let speed = current_velocity_xz.length();
 
-    if keycode.pressed(KeyCode::S) {
-        player.translation += speed * bearing_vector * -1.0;
-    }
+    let current_velocity_with_friction_xz = {
+        if speed < 0.1 {
+            Vec3::ZERO
+        } else if grounded {
+            let control = if speed > stop_speed { speed } else { stop_speed };
+            let speed_loss = control * friction * time.delta_seconds();
 
+
+            let mut new_speed = speed - speed_loss;
+
+            if new_speed < 0. { new_speed = 0.; }
+
+            let new_speed_ratio = new_speed / speed;
+
+
+            Vec3::new(
+                current_velocity.x * new_speed_ratio,
+                0.0,
+                current_velocity.z * new_speed_ratio,
+            )
+        } else {
+            Vec3::new(
+                current_velocity.x,
+                0.0,
+                current_velocity.z,
+            )
+        }
+    };
+
+    let wished_direction = {
+        let f = player_camera.forward();
+        let bearing_vector = Vec3::new(f.x, 0.0, f.z).normalize_or_zero();
+        let mut wished_direction = Vec3::ZERO;
+        if keycode.pressed(KeyCode::W) {
+            wished_direction += bearing_vector;
+        }
+
+        if keycode.pressed(KeyCode::A) {
+            wished_direction += Vec3::new(bearing_vector.z, 0.0, bearing_vector.x * -1.0);
+        }
+
+        if keycode.pressed(KeyCode::D) {
+            wished_direction += Vec3::new(bearing_vector.z * -1.0, 0.0, bearing_vector.x);
+        }
+
+        if keycode.pressed(KeyCode::S) {
+            wished_direction += bearing_vector * -1.0;
+        }
+        wished_direction.normalize_or_zero()
+    };
+    
+    //Quake style speed calculations
+    let new_velocity_xz = {
+        let current_speed = current_velocity_with_friction_xz.dot(wished_direction);
+        let add_speed = run_speed - current_speed;
+        let add_speed_with_cap = f32::max(f32::min(add_speed, max_accel * time.delta_seconds()), 0.0);
+        current_velocity_with_friction_xz + (wished_direction * add_speed_with_cap)
+    };
+
+    let jump_velocity = if keycode.pressed(KeyCode::Space) && grounded {
+        jump_force
+    } else {
+        0.0
+    };
+
+    let new_velocity_y = Vec3::new(
+        0.0, 
+        current_velocity.y - (gravity * time.delta_seconds()) + jump_velocity,
+        0.0,
+    );
+
+
+    let new_velocity = new_velocity_y + new_velocity_xz;
+
+    controller.translation = Some(new_velocity * time.delta_seconds());
+    
+
+    adjust_player_camera(
+        ev_motion,
+        player_camera,
+        window,
+        sensitivity,
+    );
+
+}
+
+fn adjust_player_camera(
+    mut ev_motion: EventReader<MouseMotion>,
+    mut player_camera: Mut<Transform>,
+    window: &Window,
+    sensitivity: f32,
+){
     for ev in ev_motion.iter() {
         let (mut yaw, mut pitch, _) = player_camera.rotation.to_euler(EulerRot::YXZ);
         // Using smallest of height or width ensures equal vertical and horizontal sensitivity
         let window_scale = window.height().min(window.width());
-        pitch -= (1.0 * ev.delta.y * window_scale / 1000.0).to_radians();
-        yaw -= (1.0 * ev.delta.x * window_scale / 1000.0).to_radians();
+        pitch -= (sensitivity * ev.delta.y * window_scale / 10000.0).to_radians();
+        yaw -= (sensitivity * ev.delta.x * window_scale / 10000.0).to_radians();
 
         pitch = pitch.clamp(-1.54, 1.54);
 
@@ -88,8 +177,6 @@ fn player_move_demo(
         player_camera.rotation =
             Quat::from_axis_angle(Vec3::Y, yaw) * Quat::from_axis_angle(Vec3::X, pitch);
     }
-
-
 }
 
 fn configure_player_entity(
@@ -105,12 +192,15 @@ fn configure_player_entity(
     println!("player found, initializing");
 
     commands.entity(entity)
-        .insert(RigidBody::Dynamic)
-        .insert(LockedAxes::ROTATION_LOCKED)
-        .insert(AdditionalMassProperties::Mass(10.0))
-        .insert(ExternalForce {
-            force: Vec3::new(0.0, 0.0, 30.0),
-            torque: Vec3::ZERO,
+        .insert(RigidBody::KinematicPositionBased)
+        .insert(Collider::ball(0.8))
+        .insert(KinematicCharacterController{
+            offset: CharacterLength::Absolute(0.5),
+            ..default()
+        })
+        .insert(Restitution {
+            coefficient: 0.2,
+            combine_rule: CoefficientCombineRule::Min,
         })
         .with_children(|parent| {
             parent
@@ -144,19 +234,13 @@ pub fn test_collision_events(
 pub struct DemoPlugin;
 impl Plugin for DemoPlugin {
     fn build(&self, app: &mut App) {
-        app.register_type::<Interactible>()
-            .register_type::<Pickable>()
-            .register_type::<SoundMaterial>()
+        app
             .register_type::<Player>()
             .register_type::<PlayerCamera>()
-            // little helper utility, to automatically inject components that are dependant on an other component
-            // ie, here an Entity with a Player component should also always have a ShouldBeWithPlayer component
-            // you get a warning if you use this, as I consider this to be stop-gap solution (usually you should have either a bundle, or directly define all needed components)
             .add_systems(
                 Update,
                 (
-                    insert_dependant_component::<Player, ShouldBeWithPlayer>,
-                    player_move_demo, //.run_if(in_state(AppState::Running)),
+                    player_move,
                     test_collision_events,
                     configure_player_entity,
                 ),
